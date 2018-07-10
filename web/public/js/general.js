23fae3c0b06a5c827fd3437263c9664da0bc2037
Jean-Louis Michael <michael.jean-louis@gfi.fr>
2018-07-10 15:03:13 +0200

    menu recherche reste en haut du navaigateur quand il y a un scroll

--- web/public/js/general.js
+++ web/public/js/general.js
@@ -16,9 +16,32 @@ $(document).ready(function()
 		$("#resp-menu").hide();
 		$("#resp-user").toggle();
 	});
+	//fly menu
+    $(window).scroll(function(){ positionMenu(); });
+    positionMenu();
 });
+var fix = 0;
 //menu flottant
-function positionDossierMainButton()
+function positionMenu()
 {
-	
+	var doc = document.documentElement;
+    var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+    if(top >= 120)
+    {
+        fix = 1;  
+        $(".srchinp").css("height", "30px");   
+        $(".srchcheck").addClass("srchcheck-fix"); 
+        $(".srchboxall").addClass("srchboxall-fix"); 
+        $(".srchbtn").addClass("srchbtn-fix"); 
+        $("#srcbox").addClass("srchbox-fix");
+    }
+    else if(fix == 1)
+    {
+        fix = 0;
+        $(".srchinp").css("height", "50px");
+        $(".srchcheck").removeClass("srchcheck-fix"); 
+        $(".srchboxall").removeClass("srchboxall-fix"); 
+        $(".srchbtn").removeClass("srchbtn-fix"); 
+        $("#srcbox").removeClass("srchbox-fix"); 
+    }
 }
