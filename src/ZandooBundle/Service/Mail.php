fa77b4dcca77504faca4e12c7fad4e80544b0957
DPRES <chirac.mbala@gmail.com>
2018-11-03 18:01:42 +0100

    resolve conflict

--- src/ZandooBundle/Service/Mail.php
+++ src/ZandooBundle/Service/Mail.php
@@ -1,22 +1,59 @@
 <?php
  namespace ZandooBundle\Service;
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ 
+ use  Symfony\Bundle\TwigBundle\TwigEngine;
+
  class Mail{
+     private $mailer;
+     public $templating;
+
+     public function __construct(\Swift_Mailer $mailer,TwigEngine $template){
+        $this->mailer = $mailer;
+        $this->templating = $template;
+     }
      
      public function sendMail($destinataire,$randPassword){
-        $subject = 'changement de mot de passe';
-        $message = 'Bonjour votre nouveau mot de passe est'.$randPassword.'\n ce ci est un mail automatique inutile d\'y repondre';
+        $subject = 'Changement de mot de passe';
+        $message = "Bonjour votre nouveau mot de passe est.$randPassword.<br/><p> ce ci est un mail automatique inutile d'y repondre <p>
+                   <br/><p> <a href='http://82.165.65.236/cmbproject/web/app.php'> Aller sur Zandoo </a></p>";
         $headers = array(
-           'From' => 'webmaster@example.com',
-           'Reply-To' => 'webmaster@example.com',
+           'From' => 'zando-admin@gmail.com',
+           'Reply-To' => 'zando-admin@gmail.com',
            'X-Mailer' => 'PHP/' . phpversion()
        );
 
        mail($destinataire, $subject, $message, 'webmaster@example.com');//$headers);
+         
+     }
+     
+     public function sendInitAccountEmail($utilisateur,$randPassword){
+        $subject = 'Création de votre compte';
+        $message = "Bonjour <br/> Votre compte est bien créé avec </br>
+                   Identifiant : ".$utilisateur->getUsername()."".
+                   "Mot de Passe : ".$randPassword ."".
+                   "<p> ce ci est un mail automatique inutile d'y repondre <p>
+                   <p><a href='http://82.165.65.236/cmbproject/web/app.php'> Aller sur Zandoo </a></p>";   
+        $headers = array(
+           'From' => 'zando-admin@gmail.com',
+           'Reply-To' => 'zando-admin@gmail.com',
+           'X-Mailer' => 'PHP/' . phpversion()
+       );
+     
+         
+//        $message = (new \Swift_Message('Création de votre compte'))              
+//        ->setFrom('zando-admin@gmail.com')
+//        ->setTo($utilisateur->getEmail())
+//        ->setBody(
+//            $this->templating->render(
+//                '@Zandoo/Emails/creationUtilisateur.html.twig',
+//                array('utilisateur' => $utilisateur)
+//            ),
+//            'text/html'
+//        );
+//       $this->mailer->send($message); 
+//       
+       
+       mail($utilisateur->getEmail(), $subject, $message, 'zando-admin@gmail.com');//$headers);
          
      }
  }
