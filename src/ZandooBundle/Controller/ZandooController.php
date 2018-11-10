5ca7574cc0b7e0b5b5301bbe0adf2d8659673355
DPRES <chirac.mbala@gmail.com>
2018-11-10 23:09:40 +0100

    espace utilisateur

--- src/ZandooBundle/Controller/ZandooController.php
+++ src/ZandooBundle/Controller/ZandooController.php
@@ -15,10 +15,10 @@ use ZandooBundle\Entity\Categorie;
 use ZandooBundle\Entity\Famille;
 use ZandooBundle\Entity\Critere;
 use ZandooBundle\Entity\Ville;
-use ZandooBundle\Service\Monnaie;
 use ZandooBundle\Entity\Signalement;
 use ZandooBundle\Entity\Motif;
 use ZandooBundle\Form\FormSignalementType;
+use ZandooBundle\Form\FormUtilisateurType;
 
 
 class ZandooController extends Controller
@@ -69,13 +69,13 @@ class ZandooController extends Controller
                             'cat'        => '',
                             'titres'     => '',
                             'urgentes'   => '',
-			                'total'      => array_merge($total,array(3,4,5,6,7,8,9,10)),
+			    'total'      => $total
                     )
             );
     }	
     
     /**
-     * @Route("/offres", name="listes_annonces") 
+     * @Route("/offres", name="offres") 
      *     
      */	
     public function listerAnnoncesAction(Request $request)
@@ -109,7 +109,7 @@ class ZandooController extends Controller
                             'cat'        => '',
                             'titres'     => '',
                             'urgentes'   => '',
-			                'total'      => array_merge($total,array(3,4,5,6,7,8,9,10)),
+			    'total'      =>$total
                     )
             );
     }	
@@ -164,7 +164,7 @@ class ZandooController extends Controller
                                 'titres'     => $titre,
                                 'urgentes'   => $urgentes,
                                 'numPage'    =>$numPage,
-                                'total'      => array_merge($total,array(3,4,5,6,7,8,9,10)),
+                                'total'      => $total
 			)
                 );
     }    
@@ -300,6 +300,27 @@ class ZandooController extends Controller
        $annonce->setActif(0);
        $retour = $annonce->getType() == 1 ?  $this->redirectToRoute('demandes'):$this->redirectToRoute('annonces');  
        return $retour;
+    }
+    /**
+     * 
+     *  @Route("comptes/{id}", name="compte_utilisateurs")
+     *  @ParamConverter("utilisateur", class="ZandooBundle:Utilisateur", isOptional=true)
+     * 
+     */
+    public function annonceByUtilisateurAction(Request $request,$utilisateur){
+        $repoAnnoce = $this->getDoctrine()->getRepository(Annonce::class); 
+        $critere = new Critere();
+        $form = $this->createForm(FormUtilisateurType::class,$utilisateur,array());
+        $critere->setIdUtilisateur($utilisateur->getId());	
+	$annonces = $repoAnnoce->findAnnonceByCritere($critere);
+        if($form->isValid() && $form->isSubmitted()){
+            //TODO
+        }
+        return $this->render('@Zandoo/Annonce/utilisateurAnnonce.html.twig',array(
+                                    'annonces'=>$annonces,
+                                    'utilisateur'=>$utilisateur,
+                                    'form'=>$form->createView()
+         )); 
     }
     /**
      * @Route("recherche", name="chercher_annonces")     
--- src/ZandooBundle/Service/Mail.php
+++ src/ZandooBundle/Service/Mail.php
@@ -1,61 +1,53 @@
 <?php
  namespace ZandooBundle\Service;
  
- use  Symfony\Bundle\TwigBundle\TwigEngine;
+ use Symfony\Bundle\TwigBundle\TwigEngine;
+ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
  class Mail{
-     private $mailer;
-     public $templating;
+    private $mailer;
+    public $templating;
 
-     public function __construct(\Swift_Mailer $mailer,TwigEngine $template){
+    public function __construct(\Swift_Mailer $mailer,TwigEngine $template){
         $this->mailer = $mailer;
         $this->templating = $template;
-     }
+    }
      
-     public function sendMail($destinataire,$randPassword){
-        $subject = 'Changement de mot de passe';
-        $message = "Bonjour votre nouveau mot de passe est.$randPassword.<br/><p> ce ci est un mail automatique inutile d'y repondre <p>
-                   <br/><p> <a href='http://82.165.65.236/cmbproject/web/app.php'> Aller sur Zandoo </a></p>";
-        $headers = array(
-           'From' => 'zando-admin@gmail.com',
-           'Reply-To' => 'zando-admin@gmail.com',
-           'X-Mailer' => 'PHP/' . phpversion()
-       );
-
-       mail($destinataire, $subject, $message, 'webmaster@example.com');//$headers);
-         
-     }
+    public function sendMail($utilisateur,$randPassword){
+  
+        $message = (new \Swift_Message('Changement de mot de passe'))              
+             ->setFrom($_SERVER["SERVER_ADMIN"])
+             ->setTo($utilisateur->getEmail())
+             ->setBody(
+                 $this->templating->render(
+                     '@Zandoo/Emails/modificationPassword.html.twig',
+                     array( 
+                             'utilisateur' =>$utilisateur,
+                             'password'=>$randPassword
+                )),'text/html');
+        $this->mailer->send($message);                    
+    }
      
-     public function sendInitAccountEmail($utilisateur,$randPassword){
-        $subject = 'Création de votre compte';
-        $message = "Bonjour <br/> Votre compte est bien créé avec </br>
-                   Identifiant : ".$utilisateur->getUsername()."".
-                   "Mot de Passe : ".$randPassword ."".
-                   "<p> ce ci est un mail automatique inutile d'y repondre <p>
-                   <p><a href='http://82.165.65.236/cmbproject/web/app.php'> Aller sur Zandoo </a></p>";   
-        $headers = array(
-           'From' => 'zando-admin@gmail.com',
-           'Reply-To' => 'zando-admin@gmail.com',
-           'X-Mailer' => 'PHP/' . phpversion()
-       );
-     
-         
-//        $message = (new \Swift_Message('Création de votre compte'))              
-//        ->setFrom('zando-admin@gmail.com')
-//        ->setTo($utilisateur->getEmail())
-//        ->setBody(
-//            $this->templating->render(
-//                '@Zandoo/Emails/creationUtilisateur.html.twig',
-//                array('utilisateur' => $utilisateur)
-//            ),
-//            'text/html'
-//        );
-//       $this->mailer->send($message); 
-//       
-       
-       mail($utilisateur->getEmail(), $subject, $message, 'zando-admin@gmail.com');//$headers);
-         
-     }
+    public function sendInitAccountEmail($utilisateur,$passWord){
+        try{   
+            $message = (new \Swift_Message('Création de votre compte'))              
+                 ->setFrom($_SERVER["SERVER_ADMIN"])
+                 ->setTo($utilisateur->getEmail())
+                 ->setBody(
+                     $this->templating->render(
+                         '@Zandoo/Emails/creationUtilisateur.html.twig',
+                         array(
+                                'utilisateur' => $utilisateur,
+                                'password'=>$passWord)
+                     ),
+                     'text/html'
+                 );
+                $this->mailer->send($message); 
+                return 1;
+            }catch(\ Exception $e){
+                return 0;
+        }             
+    }
  }
 
 
